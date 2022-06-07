ccc12c835e564fe1c07a36d0b794bd071d84ad70
Maheswaran <pystud19@gmail.com>
2022-06-07 11:37:54 +0530

    NOTES

--- handson/Day 02/Finance App - Case Study.cpp
+++ handson/Day 02/Finance App - Case Study.cpp
@@ -31,12 +31,45 @@ Project Structure:
 	(Directory Structure)
 financeapp	
 	header
-		scheme_t.h scheme_db.h 
+		scheme_t.h scheme_db.h scheme_add_ux.h
 	source 
-		scheme_db.c 
+		scheme_db.c scheme_add_ux.c employee_app.c
 	object
 	build 
-	
+...............................................................
+#/home/mahesh/financeapp
+$cd ../source
+$ gcc -I../header *.c -c
+$ ls
+$ mv *.o ../object
+$ ls
+$ ls ../object
+$ cd ../object
+$ gcc *.o -o ../build/finempapp
+$ cd ../build
+$ ./finempapp 
+Choice (1-Manage Scheme, 2-Process Loan, 3-Release Loan, 0-Exit):
+1
+Choice (1-Add Scheme, 2-Update Loan, 3-Display Schemes, 0-Exit):
+1
+Read scheme details from the user console(keyboard) here... into scheme objectCall scheme_create pass the scheme object as the argumentChoice (1-Add Scheme, 2-Update Loan, 3-Display Schemes, 0-Exit):
+0
+Choice (1-Manage Scheme, 2-Process Loan, 3-Release Loan, 0-Exit):
+0
+...............................................................
+    
+    
+...............................................................
+#/home/mahesh/financeapp
+$ cd ../source
+$ gcc -I../header *.c -c                !!! COMPILE
+$ mv *.o ../object
+$ cd ../object
+$ gcc *.o -o ../build/finempapp         !!! BUILD 
+$ cd ../build
+$ ./finempapp                           !!! RUN / EXECUTE
+...............................................................
+    
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^	
 scheme_t.h	
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -71,17 +104,81 @@ scheme_db.c
 void scheme_create(scheme_t scheme)
 {
     //code here 
+    //add into file (either as the binary or text)
 }   
 void scheme_update(scheme_t scheme)
 {
     //code here 
+    //update into file
 }   
 void scheme_read_all(scheme_t schemes[], int* scheme_size_addr)
 {
     //code here 
+    //read from file - all scheme records
 }   
 void scheme_read_byid(scheme_t *scheme_addr, int scheme_id)
 {
     //code here 
+    //read from file - one scheme record 
 }   
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+scheme_add_ux.h
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#include"scheme_db.h"
+void scheme_create_page();
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+scheme_add_ux.c
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#include"scheme_add_ux.h"
+#include<stdio.h>
+void scheme_create_page(){
+    //Declare & Define scheme object here 
+    printf("Read scheme details from the user console(keyboard) here...");
+    printf(" into scheme object");
+    //CODE HERE
+    printf("Call scheme_create pass the scheme object as the argument");
+    //CODE HERE
+    //scheme_create(/*pass sheme object*/);
+}
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+employee_app.c
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#include "scheme_add_ux.h"
+#include<stdio.h>
+void manage_scheme()
+{
+    int menu;
+    do {
+        printf("Choice (1-Add Scheme, 2-Update Loan, 3-Display Schemes, 0-Exit):\n");
+        scanf("%d",&menu);
+        
+        if(1 == menu){
+            scheme_create_page();
+        }
+    }while(1 == menu || 2 == menu || 3 == menu);
+}   
+
+int main()
+{
+    int menu;
+    
+    do {
+        printf("Choice (1-Manage Scheme, 2-Process Loan, 3-Release Loan, 0-Exit):\n");
+        scanf("%d",&menu);
+        
+        if(1 == menu){
+            manage_scheme(); 
+        }
+    }while(1 == menu || 2 == menu || 3 == menu);
+    return 0;
+}
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
