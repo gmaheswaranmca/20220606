cd3aecab399afc1c3af04843cb7284e3a64a17f3
gmaheswaranmca <gmaheswaranmca@gmail.com>
2022-07-13 13:30:44 +0530

    NOTES

--- handson/Day 24-20220713/generate-code-v3.c
+++ handson/Day 24-20220713/generate-code-v3.c
@@ -1,4 +1,4 @@
-#define JOIN(a,b) a##b
+//#define JOIN(a,b) a##b
 #define GIVE_CODE_DB_INSERT(NAME1,NAME2,NAME3) void NAME1##_db_insert(NAME2##_t* NAME2##Addr) \
 { \
     char fileName[] = #NAME3; \
@@ -10,4 +10,13 @@
     fclose(out); \
 }
 GIVE_CODE_DB_INSERT(emp,employee,emp.dat)
-JOIN(emp,dat)
+//JOIN(emp,dat)
+
+
+
+/*
+gcc generate-code-v3.c -E | sed "s/;/;\n/g"
+
+gcc generate-code-v3.c -E | sed "s/;/;\n/g" | sed "s/{/\n{\n/g" | tail -11
+
+*/
